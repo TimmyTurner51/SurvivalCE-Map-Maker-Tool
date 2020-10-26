b72a77e1d8775a20eaa024bbb3f847c966c1860a
TimmyTurner51 <46533729+TimmyTurner51@users.noreply.github.com>
2020-10-26 09:25:06 -0500

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////////////////////
+// SurvivalCE Map Creator Version DEV_0.00
+// Author: TimmyTurner62
+// License: Gnu GPLv2?
+// Description: SurvivalCE Map Creator Tool. Thanks to Michael0x18.
+////////////////////////////////////////////////////////////////////
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <tice.h>
+#include <graphx.h>
+#include <keypadc.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+uint8_t Xrooms;
+uint8_t Yrooms;
+char buffer[10];
+os_GetStringInput(char * "Map X Size: ", buffer, sizeof(buffer));
+Xrooms = 20;
+Yrooms = 20;
+uint16_t mapL[Xrooms * 20][Yrooms * 15];
+
+return;
