11ea47d590efe4b1a9b22c57a89aed93494754d2
TimmyTurner51 <46533729+TimmyTurner51@users.noreply.github.com>
2020-10-28 11:06:57 -0500

    It finally works on OS 5.3.0 (maybe on lower/higher too)

--- src/main.c
+++ src/main.c
@@ -20,6 +20,8 @@ void createVars(void);
 void input(void);
 void start(void);
 
+	static gfx_sprite_t* sprites[12] = { dirt, grass, stone, wood, wood2, water, lava, netherrack, fireball, traptile1, traptile2, sailcloth };
+	static char screenMap[20 * 15];
 
 
 void main(void) {
@@ -34,44 +36,48 @@ void start(void) {
 	uint16_t CursorY;
 	uint16_t pos;
 	uint16_t Type;
-	char screenMap[20 * 15];
 	uint16_t x;
 	uint16_t y;
 	uint16_t xa;
 	uint16_t xb;
 	uint16_t exit;
 	uint8_t appvar;
-	gfx_sprite_t* sprites[12] = { dirt, grass, stone, wood, wood2, water, lava, netherrack, fireball, traptile1, traptile2, sailcloth };
+	uint8_t redraw;
 	gfx_FillScreen(255);
 	CursorX = 0;
 	CursorY = 0;
 	Type = 0;
 	xa = 0;
 	exit = 0;
+	redraw = 1;
 	while (exit == 0) {
 		gfx_SetColor(210);
 		if (Type == 0)gfx_FillRectangle(CursorX + 6, CursorY + 6, 4, 4);
 		if (Type != 0)gfx_Sprite(sprites[Type], CursorX, CursorY);
 		if (kb_IsDown(kb_KeyUp) && CursorY > 0) {
 			//gfx_BlitScreen();
+			redraw = 1;
 			delay(100);
 			CursorY = CursorY - 16;
 			xa = xa - 20;
 		}
 		if (kb_IsDown(kb_KeyDown) && CursorY < 224) {
 			//gfx_BlitScreen();
+			redraw = 1;
 			delay(100);
 			CursorY = CursorY + 16;
 			xa = xa + 20;
 		}
 		if (kb_IsDown(kb_KeyLeft) && CursorX > 0) {
 			//gfx_BlitScreen();
+			redraw = 1;
 			delay(100);
 			CursorX = CursorX - 16;
 			xa = xa - 1;
 		}
 		if (kb_IsDown(kb_KeyRight) && CursorX < 304) {
 			//gfx_BlitScreen();
+			redraw = 1;
 			delay(100);
 			CursorX = CursorX + 16;
 			xa = xa + 1;
@@ -85,6 +91,7 @@ void start(void) {
 			Type = Type + 1;
 		}
 			if (Type > 11)Type = 0;
+
 			if (kb_IsDown(kb_KeyClear)) {
 				exit = 1;
 				ti_CloseAll();
@@ -96,6 +103,8 @@ void start(void) {
 				return;
 			}
 
+			if (redraw == 1) {
+				redraw = 0;
 				gfx_SetColor(255);
 				xb = 0;
 				for (y = 0; y < 15; y++) {
@@ -107,6 +116,7 @@ void start(void) {
 						xb = xb + 1;
 					}
 				}
+			}
 	}
 	gfx_End();
 
@@ -116,31 +126,37 @@ void start(void) {
 
 /* loading appvars… */
 void createVars(void) {
-	char screenMap[20 * 15];
 	ti_var_t appvar;
 	uint8_t xb;
 	uint8_t x;
 	uint8_t y;
-	gfx_sprite_t* sprites[12] = { dirt, grass, stone, wood, wood2, water, lava, netherrack, fireball, traptile1, traptile2, sailcloth };
 	ti_CloseAll();
 	appvar = ti_Open("SrvMap00", "r");
+
+	gfx_PrintStringXY("Point X", 5, 5);
+	while (kb_IsDown(kb_KeyClear)) {}
+
 	if (!appvar) {
-			for (x = 0; x < 20 * 15; x++) {
-				screenMap[x] = 0;
-			}
+
 		ti_CloseAll();
 		appvar = ti_Open("SrvMap00", "w");
+		gfx_PrintStringXY("Point Y", 5, 5);
+		while (kb_IsDown(kb_KeyClear)) {}
 		ti_Write(screenMap, 300, 1, appvar);
 		ti_SetArchiveStatus(1, appvar);
 		ti_CloseAll();
 	}
 	
+
 	if (appvar) {
 		ti_CloseAll();
 		appvar = ti_Open("SrvMap00", "r");
 		ti_Read(screenMap, 300, 1, appvar);
 		xb = 0;
-		start();
+
+		gfx_PrintStringXY("Point Z", 5, 5);
+		while (kb_IsDown(kb_KeyClear)) {}
+
 		gfx_SetColor(255);
 		for (y = 0; y < 15; y++) {
 			for (x = 0; x < 20; x++) {
@@ -152,6 +168,7 @@ void createVars(void) {
 		ti_CloseAll();
 	}
 	
+
 }
 
 
