241e8644a61984d70a286d37cc536449af5d82ce
TimmyTurner51 <46533729+TimmyTurner51@users.noreply.github.com>
2020-10-30 10:57:00 -0500

    Pretty much finished, may just add a few more stuff

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////
-// SurvivalCE Map Creator Version DEV_0.00
+// SurvivalCE Map Creator Version DEV_0.22
 // Author: TimmyTurner62
 // License: Gnu GPLv2?
 // Description: SurvivalCE Map Creator Tool. Thanks to Michael0x18.
@@ -20,31 +20,36 @@ void createVars(void);
 void input(void);
 void start(void);
 void drawScreen();
+void addRoom(void);
 
 	static gfx_sprite_t* sprites[12] = { dirt, grass, stone, wood, wood2, water, lava, netherrack, fireball, traptile1, traptile2, sailcloth };
-	static char screenMap[20 * 15];
-	static char wholeMap[20 * 15];
+	static char wholeMap[(20 * 14) * (15 * 14)];
 	static uint8_t screen;
-	static uint16_t CursorX;
-	static uint16_t CursorY;
-	static uint16_t pos;
-	static uint16_t Type;
-	static uint16_t x;
-	static uint16_t y;
-	static uint16_t i;
-	static uint16_t xa;
-	static uint16_t xb;
-	static uint16_t OldX;
-	static uint16_t OldY;
-	static uint16_t option;
+	static uint32_t CursorX;
+	static uint32_t CursorY;
+	static uint32_t pos;
+	static uint32_t Type;
+	static uint32_t x;
+	static uint32_t y;
+	static uint32_t i;
+	static uint32_t xa;
+	static uint32_t xb;
+	static uint32_t OldX;
+	static uint32_t OldY;
+	static uint32_t option;
 	static uint8_t appvar;
 	static uint8_t redraw;
-	static uint16_t room;
-	static uint16_t totalRooms;
+	static uint32_t room;
+	static uint32_t roomX;
+	static uint32_t roomY;
+
 
 void main(void) {
 	gfx_Begin();
 	gfx_SetPalette(mypalette, sizeof_mypalette, 0);
+	for (xa = 0; xa < sizeof(wholeMap); xa++) {
+		wholeMap[xa] = 0;
+	}
 	screen = 1;
 	createVars();
 	start();
@@ -53,14 +58,13 @@ void main(void) {
 void drawScreen(void) {
 	redraw = 0;
 	gfx_SetColor(255);
-	xb = 0;
+	xb = room - 1;
 	for (y = 0; y < 15; y++) {
 		for (x = 0; x < 20; x++) {
-			if (xb != xa) {
-				if ((screenMap[xb] > -1))gfx_Sprite(sprites[screenMap[xb]], x * 16, y * 16);
-			}
-			xb = xb + 1;
+				if ((wholeMap[xb] > -1))gfx_Sprite(sprites[wholeMap[xb]], x * 16, y * 16);
+			xb++;
 		}
+		xb += 20 * 13;
 	}
 }
 
@@ -69,68 +73,99 @@ void start(void) {
 	uint8_t start;
 	uint8_t end;
 	gfx_FillScreen(255);
-	room = 0;
 	CursorX = 0;
 	CursorY = 0;
 	Type = 0;
 	xa = 0;
 	exit = 0;
 	redraw = 1;
+	room = 1;
+	roomX = 1;
+	roomY = 1;
 	while (exit == 0) {
 		kb_Scan();
-		if (Type == 0) {
+
 			gfx_SetColor(255);
-			gfx_FillRectangle(x * 16, y * 16, 16, 16);
+			gfx_FillRectangle(CursorX, CursorY, 16, 16);
 			gfx_SetColor(210);
 			gfx_FillRectangle(CursorX + 6, CursorY + 6, 4, 4);
-		}
 
 		if (Type != 0)gfx_Sprite(sprites[Type], CursorX, CursorY);
+
 		if (kb_IsDown(kb_KeyUp) && CursorY > 0) {
 			redraw = 1;
 			delay(100);
 			CursorY = CursorY - 16;
-			xa = xa - 20;
+			xa -= 20 * 14;
 		}
 		if (kb_IsDown(kb_KeyDown) && CursorY < 224) {
 			redraw = 1;
 			delay(100);
 			CursorY = CursorY + 16;
-			xa = xa + 20;
+			xa += 20 * 14;
 		}
 		if (kb_IsDown(kb_KeyLeft) && CursorX > 0) {
 			redraw = 1;
 			delay(100);
 			CursorX = CursorX - 16;
-			xa = xa - 1;
+			xa--;
 		}
 		if (kb_IsDown(kb_KeyRight) && CursorX < 304) {
 			redraw = 1;
 			delay(100);
 			CursorX = CursorX + 16;
-			xa = xa + 1;
+			xa++;
 		}
+		
+		if (kb_IsDown(kb_Key4) && roomX > 1) {
+			redraw = 1;
+			delay(100);
+			roomX--;
+			room -= 20;
+			xa -= 20;
+		}
+		if (kb_IsDown(kb_Key6) && roomX < 14) {
+			redraw = 1;
+			delay(100);
+			roomX++;
+			room += 20;
+			xa += 20;
+		}
+		if (kb_IsDown(kb_Key8) && roomY > 1) {
+			redraw = 1;
+			delay(100);
+			roomY--;
+			room -= 4200;
+			xa -= 4200;
+		}
+		if (kb_IsDown(kb_Key2) && roomY < 14) {
+			redraw = 1;
+			delay(100);
+			roomY++;
+			room += 4200;
+			xa += 4200;
+		}
+		
 		if (kb_IsDown(kb_Key2nd)) {
 			delay(100);
-			screenMap[xa] = Type;
-			wholeMap[((totalRooms - 1) * 300) + xa] = Type;
+			wholeMap[xa] = Type;
 		}
 		if (kb_IsDown(kb_KeyAlpha)) {
 			delay(160);
 			Type = Type + 1;
 		}
-			if (Type > 11)Type = 0;
+			if (Type > 12)Type = 0;
 
 			if (kb_IsDown(kb_KeyClear)) {
 				exit = 1;
 				ti_CloseAll();
-				start = ((totalRooms - 1) * 300); 
+				//start = ((totalRooms - 1) * 300); 
 				//If you want to copy to the same position in the first array, you can use `memcpy(&screenMap[start], &wholeMap[start], (end - start) * sizeof wholeMap[0]);`
-				for (xb = start; xb < start + 300; xb++) {
-					memcpy(&screenMap[xb], &wholeMap[xb], 1);
-				}
+				//for (xb = start; xb < start + 300; xb++) {
+				//	memcpy(&screenMap[xb], &wholeMap[xb], 1);
+				//}
 				appvar = ti_Open("SrvMap00", "w");
-				ti_Write(wholeMap, totalRooms * 300, 1, appvar);
+				ti_Write(wholeMap, sizeof(wholeMap), 1, appvar);
 				ti_SetArchiveStatus(1, appvar);
 				ti_CloseAll();
 				gfx_End();
@@ -142,7 +177,6 @@ void start(void) {
 			}
 			if kb_IsDown(kb_KeyWindow) {
 				//Map Editor Tools Menu...
-				redraw = 1;
 				y = 140;
 				i = y;
 				for (OldY = 0; OldY < 15; OldY++) {
@@ -155,7 +189,8 @@ void start(void) {
 						gfx_Sprite(wood, OldX, OldY);
 					}
 				}
-				while (!kb_IsDown(kb_KeyClear)) {
+				option = 0;
+				while (option == 0) {
 					kb_Scan();
 					if (redraw != 0) {
 						redraw = 0;
@@ -164,9 +199,9 @@ void start(void) {
 							gfx_Sprite(wood, xb, i);
 						}
 						/* button text */
-						gfx_PrintStringXY("Add Room", 148, 142);
-						//gfx_PrintStringXY("Option", 118, 162);
-						//gfx_PrintStringXY("Option", 148, 182);
+						gfx_PrintStringXY("Fill Room", 118, 142);
+						gfx_PrintStringXY("Option", 118, 162);
+						gfx_PrintStringXY("Back to Editor", 108, 182);
 						gfx_SetColor(2);
 						gfx_Rectangle(60, y, 192, 16);
 						gfx_Rectangle(61, y + 1, 190, 14);
@@ -183,8 +218,34 @@ void start(void) {
 						y = y + 20;
 					}
 					if (kb_IsDown(kb_Key2nd) && (y = 140)) option = 1;
+					if (kb_IsDown(kb_Key2nd) && (y = 160)) option = 2;
+					if (kb_IsDown(kb_Key2nd) && (y = 180)) option = 3;
 		
 				}
+				if (option = 1) {
+					for (OldY = 0; OldY < 15; OldY++) {
+						for (OldX = 0; OldX < 20; OldX++) {
+							gfx_Sprite(wood, OldX * 16, OldY * 16);
+						}
+					}
+					for (OldY = 100; OldY < 200; OldY += 20) {
+						for (OldX = 60; OldX < 244; OldX += 16) {
+							gfx_Sprite(stone, OldX, OldY);
+						}
+					}
+
+					option = 0;
+					while (!kb_IsDown(kb_Key2nd)) {
+						kb_Scan();
+						gfx_ScaledSprite_NoClip(sprites[option], 64, 104, 2, 2);
+						if (option < 0) option = 11;
+						if (option >11) option = 0;
+						if (kb_IsDown(kb_KeyLeft)) option--;
+						if (kb_IsDown(kb_KeyRight)) option++;
+					}
+
+				}
+				redraw = 1;
 
 			}
 
@@ -198,7 +259,6 @@ void start(void) {
 }
 
 
-
 /* loading appvars… */
 void createVars(void) {
 	ti_var_t appvar;
@@ -208,7 +268,7 @@ void createVars(void) {
 	if (!appvar) {
 		ti_CloseAll();
 		appvar = ti_Open("SrvMap00", "w");
-		ti_Write(wholeMap, sizeof(wholeMap) + 300, 1, appvar);
+		ti_Write(wholeMap, sizeof(wholeMap), 1, appvar);   //+300
 		ti_SetArchiveStatus(1, appvar);
 		ti_CloseAll();
 	}
@@ -218,13 +278,8 @@ void createVars(void) {
 		ti_CloseAll();
 		appvar = ti_Open("SrvMap00", "r");
 		ti_Read(wholeMap, ti_GetSize(appvar), 1, appvar);
-		totalRooms = sizeof(wholeMap) / 300;		// we could use sizeof(appvar) instead, but I think sizeof(appvar) is 317, whereas sizeof(wholeMap) is 300, maybe...
-
 		ti_CloseAll();
 		xb = 0;
-		for (x = 0; x < sizeof(wholeMap); x++) {
-			screenMap[x] = wholeMap[x];
-		}
 		gfx_SetColor(255);
 		drawScreen();
 	}
